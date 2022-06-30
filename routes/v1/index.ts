be69f4072767a4e3ef51ae86ffcbebcfca18caf9
Alaa Desouky <alaa.desouky33@gmail.com>
2022-06-30 11:04:58 +0200

    Update index.ts

--- routes/v1/index.ts
+++ routes/v1/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import authRoutes from './auth.route'
 import restaurantRoutes from './restaurant.route'
+import userRoutes from './user.route'
 
 const v1Router = Router()
 v1Router.use('/auth', authRoutes)
 v1Router.use('/restaurant', restaurantRoutes)
+v1Router.use('/user', userRoutes)
 
 export default v1Router
